fd1af09901f4fbedba2cf67aa16366230344c67c
febin.thomas <febin.thomas@cgi.com>
2019-09-23 17:03:37 +0530

    Task1 Done

---
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -0,0 +1,28 @@
+package com.stackroute.domain;
+
+public class Actor {
+    private String actorName;
+    private String actorGender;
+    private int actorAge;
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public void setActorGender(String actorGender) {
+        this.actorGender = actorGender;
+    }
+
+    public void setActorAge(int actorAge) {
+        this.actorAge = actorAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "actorName='" + actorName + '\'' +
+                ", actorGender='" + actorGender + '\'' +
+                ", actorAge=" + actorAge +
+                '}';
+    }
+}
